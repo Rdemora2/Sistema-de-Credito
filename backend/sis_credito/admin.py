abd2b2de5a4d1d584930e310f30e3f38ae8c7381
rdemora2 <rdemora2@gmail.com>
2023-08-31 12:38:56 -0300

    FEAT/ exibe nome e repsosta da api no admin

--- backend/sis_credito/admin.py
+++ backend/sis_credito/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import CreditForm
 
-admin.site.register(CreditForm)
+class CreditFormAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'credit_api_response')
+    readonly_fields = ('credit_api_response',)
+
+admin.site.register(CreditForm, CreditFormAdmin)
